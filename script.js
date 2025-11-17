ae7610182097312ca75cb7be7d6c16079f1ad23a
mdara67 <scorpionfinishwin677@gmail.com>
2025-11-17 16:18:54 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function togglemode() {
+    const html = document.documentElement
+   /*
+    if (html.classList.contains('light')) {
+        html.classList.remove('light')
+    } else {
+        html.classList.add('light')
+    }
+        */
+    html.classList.toggle('light')
+
+    const img = document.querySelector("#profile img")
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './assets/assets/avatar-light.png')
+    } else {
+        img.setAttribute('src', './assets/assets/avatar.png')
+    }
+
+    /*tem que pegar a tag img é substituir-la, caso tenha light mode, adicionar a imagem correspondente, caso tiver night agregar imagem correspondente*/
+}
